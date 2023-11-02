3d78df67c76ed8e08dae1805234da6d83401b54f
Pyarel <pyarel.fer@gmail.com>
2023-11-02 18:17:57 -0400

    Event Capturing and Event Bubbling

--- script.js
+++ script.js
@@ -55,3 +55,70 @@ console.log(getComputedStyle(message).height);
 
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 20 + 'px';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect()); //Get the x and y coordinates between the element and top of the viewport
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset); // Get the X and Y coordinates of current scroll
+
+  console.log(
+    'Height/Width Viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  ); // Get the current viewport height and width
+
+  //Scrolling
+
+  // window.scrollTo(s1coords.left, s1coords.top);
+
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//Creating a function to generate random integers
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+// Generate random colors using rgb function which calls randomInt to generate values between 0 - 255
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+// Add the event listener to nav link
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget == this);
+
+  //Stop event propagation
+  // e.stopPropagation();
+});
+// Add the event listener to nav links (ul element)
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+// Add the event listener to nav (nav element)
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  },
+  true
+); // Event Capturing is true, Event bubbling is set to false
